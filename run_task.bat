d088c1be1a8c898a6708beaf2312b4d03ad13b39
matildagibbons <141175927+matildagibbons@users.noreply.github.com>
2024-06-03 13:13:50 -0400

    new

--- run_task.bat
+++
@@ -1,2 +0,0 @@
-conda activate DynamicRoutingTaskDev
-python "C:\Users\teenspirit\Desktop\Behavior\Tilda\Stimuli\Behaviour\DynamicRoutingTask\DynamicRouting1.py" "C:\Users\teenspirit\Desktop\Behavior\Tilda\Stimuli\Behaviour\DynamicRoutingTask\taskParams.json"
