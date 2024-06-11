f52cc96e9fcbaf32c2e5d2d9245df2fed03732d4
geffenlab-npx <tildagibbons66@gmail.com>
2024-06-11 10:17:19 -0400

    Revert "sam;s suggested changes to gui and task control"
    This reverts commit 0214718ceb471810e68416b18bbf610ceae94c69.

--- TildaGUI.py
+++ TildaGUI.py
@@ -1,44 +1,27 @@
 import tkinter as tk
 import subprocess
 import os
-import json, time
 
- 
 def run_script(task_version, mouse_number):
     conda_env = "DynamicRoutingTaskDev"  # Hardcoded Conda environment
     script_path = "C:\\Users\\teenspirit\\Desktop\\Behavior\\Tilda\\Stimuli\\Behaviour\\DynamicRoutingTask\\DynamicRouting1.py"  # Hardcoded script path
     params_file = f"C:\\Users\\teenspirit\\Desktop\\Behavior\\Tilda\\Stimuli\\Behaviour\\DynamicRoutingTask\\{task_version.lower()}"  # Construct parameters file path
-
+    save_dir = f"C:\\Users\\teenspirit\\Desktop\\Behavior\\Tilda\\Behavior data\\Data\\{mouse_number}"  # Construct save directory path with mouse number
+   
+     # Create the directory if it doesn't exist
+    os.makedirs(save_dir, exist_ok=True)
     
     # Activate Conda environment
     activate_cmd = f'conda activate {conda_env} && '
     
     # Command to execute the Python script with parameters
-    python_cmd = f'python "{script_path}" "{params_file}"'  # Include save_dir as a parameter
+    python_cmd = f'python "{script_path}" "{params_file}" "{save_dir}"'  # Include save_dir as a parameter
     
     # Combine activation and script execution commands
     full_cmd = activate_cmd + python_cmd
     
     # Execute the command in a subprocess
     subprocess.run(full_cmd, shell=True)
-    
-    
-with open(params_file, 'r') as f:
-    params = json.load(f)
-
-mouse_id = get mouse_id from entry_mouse as string
-start_time = time.strftime('%Y%m%d_%H%M%S', time.localtime()) # add this to file name so you don't accidentally overwrite something
-
- 
-params['subjectName'] = mouse_id
-
-params['taskVersion'] = get task version from gui
-
-params['savePath'] = os.path.join(save_dir, mouse_id + '_' + start_time + '.hdf5')
-
-
-with open(params_file, 'w') as f:
-        json.dump(params, f)
 
 def get_task_versions(task_type):
     task_versions = []
