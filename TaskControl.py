9273008ac7365e1331a0d6470d46d5f2ecf731a0
geffenlab-npx <tildagibbons66@gmail.com>
2024-06-11 12:34:44 -0400

    guiedit

--- TaskControl.py
+++ TaskControl.py
@@ -242,8 +242,8 @@ class TaskControl():
                         self.rotaryEncoderSerialPort = 'COM4'
                         self.soundCalibrationFit = (28.655615630746905,-3.5166732104004796,61.36404105849515)
                 elif self.rigName == 'Tilda':
-                    self.saveDir = None # directory where TaskControl will create an output file
-                    self.savePath = params['savePath'] # full path to output file
+                    self.saveDir = None # directory where TaskControl will create an output file 
+                    self.savePath = params['savePath']
                     self.screen = 0
                     self.monWidth = 52.0
                     self.monDistance = 15.3
--- TildaGUI.py
+++ TildaGUI.py
@@ -1,13 +1,16 @@
 import tkinter as tk
 import subprocess
 import os
+import json
+import time
+
 
 def run_script(task_version, mouse_number):
     conda_env = "DynamicRoutingTaskDev"  # Hardcoded Conda environment
     script_path = "C:\\Users\\teenspirit\\Desktop\\Behavior\\Tilda\\Stimuli\\Behaviour\\DynamicRoutingTask\\DynamicRouting1.py"  # Hardcoded script path
     params_file = f"C:\\Users\\teenspirit\\Desktop\\Behavior\\Tilda\\Stimuli\\Behaviour\\DynamicRoutingTask\\{task_version.lower()}"  # Construct parameters file path
     save_dir = f"C:\\Users\\teenspirit\\Desktop\\Behavior\\Tilda\\Behavior data\\Data\\{mouse_number}"  # Construct save directory path with mouse number
-   
+
      # Create the directory if it doesn't exist
     os.makedirs(save_dir, exist_ok=True)
     
@@ -22,6 +25,16 @@ def run_script(task_version, mouse_number):
     
     # Execute the command in a subprocess
     subprocess.run(full_cmd, shell=True)
+    
+    with open(params_file, 'r') as f:
+        params = json.load(f)
+    mouse_id = entry_mouse.get()  # Retrieve mouse ID from entry field
+    start_time = time.strftime('%Y%m%d_%H%M%S', time.localtime()) # Generate timestamp
+    params['savePath'] = os.path.join(save_dir, f"{mouse_id}_{start_time}.hdf5")
+        
+    with open(params_file, 'w') as f:
+        json.dump(params, f)
+
 
 def get_task_versions(task_type):
     task_versions = []
@@ -56,6 +69,6 @@ visual_button.grid(row=0, column=0, columnspan=2)
 auditory_button = tk.Button(root, text="Auditory", command=lambda: create_task_buttons(root, 1, 'aud'))
 auditory_button.grid(row=0, column=2, columnspan=2)
 
+
 # Start the GUI event loop
 root.mainloop()
-
