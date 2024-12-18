e66128259227486cfd77fecf57e09df78b35022b
geffenlab-npx <tildagibbons66@gmail.com>
2024-12-18 13:42:06 -0500

    jbhv

--- RFMapping.py
+++ RFMapping.py
@@ -177,7 +177,8 @@ class RFMapping(TaskControl):
 
 if __name__ == "__main__":
     import sys,json
-    paramsPath = sys.argv[1]
+    paramsPath = "C:\Users\\teenspirit\\Desktop\\Behavior\\Tilda\\Stimuli\\Behaviour\\DynamicRoutingTask\\taskParams.json"
+   # paramsPath = sys.argv[1]
     with open(paramsPath,'r') as f:
         params = json.load(f)
     task = RFMapping(params)
