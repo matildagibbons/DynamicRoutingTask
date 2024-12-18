41bd7a84dc2cec4cd1e4c2d12d2e204dd729a3b8
geffenlab-npx <tildagibbons66@gmail.com>
2024-12-18 12:40:07 -0500

    gn

--- TaskUtils.py
+++ TaskUtils.py
@@ -114,9 +114,7 @@ def makeSoundArray(soundType,sampleRate,dur,hanningDur,vol,freq,AM=None,seed=Non
     elif soundType in ('noise','AM noise'):
         rng = np.random.RandomState(seed)
         soundArray = 2 * rng.random(t.size) - 1
-     #might be messing with my code   b,a = scipy.signal.butter(10,freq,btype='bandpass',fs=sampleRate)
-      #same  soundArray = scipy.signal.filtfilt(b,a,soundArray)
-        #again soundArray = np.ascontiguousarray(soundArray)
+        soundArray = np.ascontiguousarray(soundArray)
     if AM is not None and ~np.isnan(AM) and AM > 0:
         soundArray *= (np.sin(1.5*np.pi + 2*np.pi*AM*t) + 1) / 2
     elif hanningDur > 0:
